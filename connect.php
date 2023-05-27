86523a3d76ee0f3d6b7e82937ee4760f8c14ffa7
lapyae <lapyae@lapyaes-MacBook-Air.local>
2023-05-27 12:46:43 +0630

    database change to server

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
 $host="localhost";
-$dbname="exercies";
+$dbname="lapyae";
 $username = "root";
-$password = "password";
+$password = "Hello*111#";
 $db_option =[
     PDO::ATTR_ERRMODE=> PDO::ERRMODE_EXCEPTION, 
     PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC
